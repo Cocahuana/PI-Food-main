6cecf9a89f1251481f4f0f9155450cdfb88d16c1
Ezequiel <ezequieldominguez11@gmail.com>
2022-07-14 16:28:51 -0300

    fix Before upload to heroku

--- client/src/index.js
+++ client/src/index.js
@@ -3,22 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {store} from "./store";
-import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import dotenv from 'dotenv';
+import axios from 'axios';
 
+dotenv.config();
 
 //Envolver la app en redux porque sino redux no va a funcionar como debería
 
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
+
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-    <Router>
-        <App />
-    </Router>
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<React.StrictMode>
+			<Router>
+				<App />
+			</Router>
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
