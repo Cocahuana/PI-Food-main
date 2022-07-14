1e7995f18eba21a654753da4bd9d8d2cbacfc10e
Ezequiel <ezequieldominguez11@gmail.com>
2022-07-14 13:09:29 -0300

    feat(api/src/components/RecipeDetail.jsx): added createInDb h3 and models

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,42 +3,41 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  // Usar UUID para no sobreescribir lo que
-  // llega de la api con la receta creada en la BD
-  sequelize.define('recipe', {
-    id:{
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },
-    title: {
-      type: DataTypes.STRING(40),
-      allowNull: false,
-    },
-    summary:{
-      type: DataTypes.STRING(800),
-      allowNull: false,
-    },
-    img:{
-      type: DataTypes.STRING(500),
-      allowNull: false,
-    },
-    healthScore:{
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    stepFromDb:{
-      type: DataTypes.STRING(1024),
-      allowNull: true,
-    },
-    //Separamos lo recibido de la API de lo creado en la BD
-    createdInDb:{
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: 'Created by Ezequiel',
-    }
-
-  });
+	// defino el modelo
+	// Usar UUID para no sobreescribir lo que
+	// llega de la api con la receta creada en la BD
+	sequelize.define('recipe', {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			allowNull: false,
+			primaryKey: true,
+		},
+		title: {
+			type: DataTypes.STRING(40),
+			allowNull: false,
+		},
+		summary: {
+			type: DataTypes.STRING(800),
+			allowNull: false,
+		},
+		img: {
+			type: DataTypes.STRING(500),
+			allowNull: false,
+		},
+		healthScore: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+		},
+		stepFromDb: {
+			type: DataTypes.STRING(1024),
+			allowNull: true,
+		},
+		//Separamos lo recibido de la API de lo creado en la BD
+		createdInDb: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			defaultValue: 'Created by Me',
+		},
+	});
 };
--- client/src/components/RecipeDetail.jsx
+++ client/src/components/RecipeDetail.jsx
@@ -1,93 +1,113 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getDetails } from "../actions";
-import { useEffect } from "react";
-import "./RecipeDetail.css";
+import { getDetails } from '../actions';
+import { useEffect } from 'react';
+import './RecipeDetail.css';
 
-export default function RecipeDetail(props){
-    const dispatch = useDispatch();
-    const id = props.match.params.id; 
-    const loading = useSelector ((state) => state.loading);
+export default function RecipeDetail(props) {
+	const dispatch = useDispatch();
+	const id = props.match.params.id;
+	const loading = useSelector((state) => state.loading);
 
-    useEffect(() => {
-        dispatch(getDetails(id));
-    }, [dispatch, id])
+	useEffect(() => {
+		dispatch(getDetails(id));
+	}, [dispatch, id]);
 
-    const myRecipe = useSelector((state) => state.recipesDetail)
+	const myRecipe = useSelector((state) => state.recipesDetail);
 
-    if(loading){
-        return(
-            <div className="details-grid" key={id}>
-                <h2 className="error-msg">Loading...</h2>
-            </div>
-        );
-    }
+	if (loading) {
+		return (
+			<div className='details-grid' key={id}>
+				<h2 className='error-msg'>Loading...</h2>
+			</div>
+		);
+	}
 
-    return(
-        <div className="details-grid" key={id}>
-            <div className="details-container">
-                <div className="detail-container">
-                    <img className="detail-img" src={myRecipe.img} alt="img not found" width="400px" height="500px"/>
-                    <div className="info-aside">
-                        <div className="title-container">
-                            <h1 className="detail-title">{myRecipe.title}</h1>
-                            <h3 className="detail-healthScore">{myRecipe.healthScore}</h3>
-                        </div>
-                        <div className="diets-container">
-                            <h3 className="diets-h3">Diet Type</h3>
-                            <div className="showDiet">
-                                <div className="border"></div>
-                                    {
-                                        //Es importante el ? porque así el map no es undefined. Como que le da tiempo a la api a traer la info
-                                        myRecipe.dietTypes?.map((e) => (
-                                            <div className="diets-h5-container" key={e}>
-                                                <h3 className="diets-h5" key={e}>{e}</h3>
-                                            </div>
-                                        ))
-                                        
-                                    }
-                                <div className="border"></div>
-                                <p>{myRecipe.createdInDb}</p>
-                            </div>
-                        </div>
-                        {/*inner atribute for html dangerouslySetInnerHTML, is a property that you can use on HTML elements in a React application to programmatically set their content*/}
-                        <div className="summary-container">
-                            <h3 className="detail-summary-h3">Summary</h3>
-                            <div className="border"></div>
-                            <p className="detail-summary-p"><div dangerouslySetInnerHTML={{ __html: myRecipe.summary }}/></p>
-                            <div className="border"></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="steps-container">
-                    <h3 className="steps-h3">Steps</h3>
-                    <ul className="steps">
-                        {
-                            
-                            Array.isArray(myRecipe.analyzedInstructions) ? 
-                            myRecipe.analyzedInstructions.map(e => {
-                                return(
-                                    <div className="step">
-                                        <p><strong>{e.number + ") "}</strong></p>
-                                        <li key={e.number}>{e.step}</li>
-                                    </div>
-                                    )
-                            }) :
-                            //Si no tengo analyzedInstructions, renderizo los que contengan steps
-                            //myRecipe.steps
-                            <li>{myRecipe.stepFromDb}</li>
-                        }
-                    </ul>
-                    <div className="goBack">
-                        <Link to= '/home'>
-                            <button className="CTA-goBack">
-                                Volver
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className='details-grid' key={id}>
+			<div className='details-container'>
+				<div className='detail-container'>
+					<img
+						className='detail-img'
+						src={myRecipe.img}
+						alt='img not found'
+						width='400px'
+						height='500px'
+					/>
+					<div className='info-aside'>
+						<div className='title-container'>
+							<h1 className='detail-title'>{myRecipe.title}</h1>
+							<h3 className='detail-healthScore'>
+								{myRecipe.healthScore}
+							</h3>
+						</div>
+						<div className='diets-container'>
+							<h3 className='diets-h3'>Diet Type</h3>
+							<div className='showDiet'>
+								<div className='border'></div>
+								{
+									//Es importante el ? porque así el map no es undefined. Como que le da tiempo a la api a traer la info
+									myRecipe.dietTypes?.map((e) => (
+										<div
+											className='diets-h5-container'
+											key={e}>
+											<h3 className='diets-h5' key={e}>
+												{e}
+											</h3>
+										</div>
+									))
+								}
+								<div className='border'></div>
+								<div className='diets-h5-container'>
+									<h3 className='diets-h5'>
+										{myRecipe.createdInDb}
+									</h3>
+								</div>
+							</div>
+						</div>
+						{/*inner atribute for html dangerouslySetInnerHTML, is a property that you can use on HTML elements in a React application to programmatically set their content*/}
+						<div className='summary-container'>
+							<h3 className='detail-summary-h3'>Summary</h3>
+							<div className='border'></div>
+							<p className='detail-summary-p'>
+								<div
+									dangerouslySetInnerHTML={{
+										__html: myRecipe.summary,
+									}}
+								/>
+							</p>
+							<div className='border'></div>
+						</div>
+					</div>
+				</div>
+				<div className='steps-container'>
+					<h3 className='steps-h3'>Steps</h3>
+					<ul className='steps'>
+						{Array.isArray(myRecipe.analyzedInstructions) ? (
+							myRecipe.analyzedInstructions.map((e) => {
+								return (
+									<div className='step'>
+										<p>
+											<strong>{e.number + ') '}</strong>
+										</p>
+										<li key={e.number}>{e.step}</li>
+									</div>
+								);
+							})
+						) : (
+							//Si no tengo analyzedInstructions, renderizo los que contengan steps
+							//myRecipe.steps
+							<li>{myRecipe.stepFromDb}</li>
+						)}
+					</ul>
+					<div className='goBack'>
+						<Link to='/home'>
+							<button className='CTA-goBack'>Volver</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
